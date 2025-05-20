3b81b3b56ace5059439e3102faa484b4fe8a0a1c
unknown-dlt <t1ger.bit22@gmail.com>
2025-05-20 11:13:36 +0300

    Add files via upload

---
+++ coderun/code1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1coderun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] xy = Console.ReadLine().Split();
+            byte x = Convert.ToByte(xy[0]);
+            byte y = Convert.ToByte(xy[1]);
+
+            Console.WriteLine(x + y);
+
+        }
+    }
+}
