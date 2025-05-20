47f22f74a1589f28b75a98418142c83d0fc9452c
unknown-dlt <t1ger.bit22@gmail.com>
2025-05-20 11:17:09 +0300

    Add files via upload

---
+++ massif/massif.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] mas = {
+                { { 1, 2 },{ 3, 4 } },
+                { { 4, 5 }, { 6, 7 } },
+                { { 7, 8 }, { 9, 10 } },
+                { { 10, 11 }, { 12, 13 }
+                }
+              };
+
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    for (int k = 0; k < mas.GetLength(2); k++)
+                    {
+                        Console.Write(mas[i, j, k] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
