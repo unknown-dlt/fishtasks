3952c5e0abdcaad4184aec418dfb24ba7030dca1
unknown-dlt <t1ger.bit22@gmail.com>
2025-05-20 11:01:47 +0300

    Add files via upload

---
+++ task3/dz4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4coderun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int[] tags = new int[a + 1];
+
+            tags[1] = 1;
+            if (a >= 2)
+                tags[2] = 1;
+
+            for (int i = 3; i <= a; i++)
+            {
+                tags[i] = tags[i - 1] + tags[i - 2];
+            }
+
+            int sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                sum += tags[i];
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
---
+++ task3/dz5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5coderun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+
+            double z = b * b - 4 * a * c;
+
+            if (z == 0)
+            {
+                double x = -b / (2.0 * a);
+                Console.WriteLine(x);
+            }
+            else
+            {
+                double sqrtD = Math.Sqrt(z);
+                double x1 = (-b + sqrtD) / (2 * a);
+                double x2 = (-b - sqrtD) / (2 * a);
+                Console.WriteLine($"{x1} {x2}");
+            }
+        }
+    }
+}
